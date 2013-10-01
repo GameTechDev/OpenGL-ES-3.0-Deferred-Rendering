f70d0be6e1134e4738742d9854cef0944e29317f
Kyle Weicht <kyle@awesomekyle.com>
2013-10-01 16:23:57 -0700

    Converter work

--- tools/exporter.cpp
+++ tools/exporter.cpp
@@ -12,6 +12,7 @@ extern "C" {
 #include <string>
 #include <map>
 #include <stdio.h>
+#include <sstream>
 
 /* Constants
  */
@@ -32,6 +33,37 @@ struct Vertex
     Vec3    tangent;
     Vec3    bitangent;
     Vec2    texcoord;
+};
+struct MeshData
+{
+    char        name[128];
+    Vertex*     vertices;
+    uint32_t    indices;
+    uint32_t    vertex_count;
+    uint32_t    index_count;
+};
+struct MaterialData
+{
+    char        name[128];
+    char        albedo_tex[128];
+    char        normal_tex[128];
+    Vec3        specular_color;
+    float       specular_power;
+    float       specular_coefficient;
+};
+struct ModelData
+{
+    char    mesh_name[128];
+    char    material_name[128];
+};
+struct SceneData
+{
+    MeshData*       meshes;
+    MaterialData*   materials;
+    ModelData*      models;
+    uint32_t        num_meshes;
+    uint32_t        num_materials;
+    uint32_t        num_models;
 };
 
 /* Variables
@@ -61,6 +93,76 @@ static void free_file_data(void* data)
 {
     free(data);
 }
+static void _load_mtl_file(const char* filename, SceneData* scene)
+{
+    char* file_data = NULL;
+    char* original_data = NULL;
+    size_t file_size = 0;
+    int matches;
+
+    load_file_data(filename, (void**)&file_data, &file_size);
+    original_data = file_data;
+
+
+    //
+    // Count materials first
+    //
+    int orig_num_materials = scene->num_materials;
+    while(1) {
+        if(file_data == NULL)
+            break;
+        char line[1024] = {0};
+        file_data = (char*)get_line_from_buffer(line, sizeof(line), file_data);
+        char line_header[64] = {0};
+        sscanf(line, "%s", line_header);
+        
+        if(strcmp(line_header, "newmtl") == 0) {
+            scene->num_materials++;
+        }
+    }
+    file_data = original_data;
+
+    //
+    // Allocate materials
+    //
+    scene->materials = (MaterialData*)realloc(scene->materials, scene->num_materials*sizeof(MaterialData));
+    MaterialData* current_material = (scene->materials - 1) + orig_num_materials;
+
+    //
+    // Loop through and create materials
+    //
+    while(1) {
+        if(file_data == NULL)
+            break;
+        char line[1024] = {0};
+        file_data = (char*)get_line_from_buffer(line, sizeof(line), file_data);
+        char line_header[64] = {0};
+        sscanf(line, "%s", line_header);
+
+        if(strcmp(line_header, "newmtl") == 0) {
+            current_material++;
+            matches = sscanf(line, "%s %s\n", line_header, current_material->name);
+            current_material->specular_power = 16.0f;
+            assert(matches == 2);
+        } else if(strcmp(line_header, "map_Kd") == 0) {
+            matches = sscanf(line, "%s %s\n", line_header, current_material->albedo_tex);
+            assert(matches == 2);
+        } else if((strcmp(line_header, "map_bump") == 0 || strcmp(line_header, "map_bump") == 0) && current_material->normal_tex[0] == '\0') {
+            matches = sscanf(line, "%s %s\n", line_header, current_material->normal_tex);
+            assert(matches == 2);
+        } else if(strcmp(line_header, "Ks") == 0) {
+            Vec3 spec_color;
+            matches = sscanf(line, "%s %f %f %f\n", line_header, &spec_color.x, &spec_color.y, &spec_color.z);
+            assert(matches == 4);
+            current_material->specular_color = spec_color;
+        } else if(strcmp(line_header, "Ns") == 0) {
+            matches = sscanf(line, "%s %f\n", line_header, &current_material->specular_coefficient);
+            assert(matches == 2);
+        }
+    }
+    free_file_data(original_data);
+}
+
 static Vertex* calculate_tangets(const SimpleVertex* vertices, int num_vertices, const void* indices, size_t index_size, int num_indices)
 {
     Vertex* new_vertices = new Vertex[num_vertices];
@@ -134,22 +236,40 @@ struct int3 {
         return false;
     }
 };
-static void _load_obj(const char* filename)
+struct Triangle
+{
+    int3    vertex[3];
+};
+//FILE* mesh_file = fopen((mesh_name + ".mesh").c_str(), "wb");
+//
+//// mtl_name_len
+//uint32_t material_name_len = (uint32_t)strlen(mesh_material_pairs[(uint32_t)jj].c_str());
+//fwrite(&material_name_len, 1, sizeof(material_name_len), mesh_file);
+//// mtl_name
+//fwrite(mesh_material_pairs[(uint32_t)jj].c_str(), 1, material_name_len, mesh_file);
+//// vertex count
+//fwrite(&vertex_count, 1, sizeof(vertex_count), mesh_file);
+//// index count
+//fwrite(&index_count, 1, sizeof(index_count), mesh_file);
+//// vertices
+//fwrite(new_vertices, sizeof(Vertex), vertex_count, mesh_file);
+//// indices
+//fwrite(&i[0], sizeof(uint32_t), index_count, mesh_file);
+//
+//fclose(mesh_file);
+
+static void _load_obj(const char* filename, SceneData* scene)
 {
     std::vector<Vec3> positions;
     std::vector<Vec3> normals;
     std::vector<Vec2> texcoords;
 
-    std::vector<std::vector<int3> >  all_indices;
+    std::vector<std::vector<Triangle> >  all_triangles;
     std::vector<std::string>  mesh_material_pairs;
     std::vector<std::string>  mesh_names;
     int current_indices = -1;
 
     //std::map<std::string, Material> all_materials;
-
-    positions.reserve(100000);
-    normals.reserve(100000);
-    texcoords.reserve(100000);
 
     int textured = 0;
 
@@ -164,6 +284,53 @@ static void _load_obj(const char* filename)
     load_file_data(filename, (void**)&file_data, &file_size);
     original_data = file_data;
 
+    int orig_num_meshes = scene->num_meshes;
+    int orig_num_models = scene->num_models;
+
+    //
+    // Count everything
+    //
+    uint32_t num_total_vertices = 0;
+    uint32_t num_total_texcoords = 0;
+    uint32_t num_total_normals = 0;
+    uint32_t num_meshes = 0;
+    while(1) {
+        if(file_data == NULL)
+            break;
+        char line[1024] = {0};
+        file_data = (char*)get_line_from_buffer(line, sizeof(line), file_data);
+        char line_header[16] = {0};
+        sscanf(line, "%s", line_header);
+
+        if(strcmp(line_header, "v") == 0) {
+            num_total_vertices++;
+        } else if(strcmp(line_header, "vt") == 0) {
+            num_total_texcoords++;
+        } else if(strcmp(line_header, "vn") == 0) {
+            num_total_normals++;
+        } else if(strcmp(line_header, "usemtl") == 0) {
+            num_meshes++;
+            scene->num_meshes++;
+            scene->num_models++;
+        } else if(strcmp(line_header, "mtllib") == 0 ) {
+            char mtl_filename[256];
+            matches = sscanf(line, "%s %s\n", line_header, mtl_filename);
+            assert(matches == 2);
+            _load_mtl_file(mtl_filename, scene);
+        }
+    }
+    original_data = file_data;
+    positions.reserve(num_total_vertices);
+    normals.reserve(num_total_normals);
+    texcoords.reserve(num_total_texcoords);
+    all_triangles.resize(num_meshes);
+
+    scene->meshes = (MeshData*)realloc(scene->meshes, sizeof(ModelData)*scene->num_meshes);
+    scene->models = (ModelData*)realloc(scene->models, sizeof(ModelData)*scene->num_models);
+
+    //
+    // Fill out vertex data
+    //
     while(1) {
         if(file_data == NULL)
             break;
@@ -189,18 +356,53 @@ static void _load_obj(const char* filename)
             matches = sscanf(line, "%s %f %f %f\n", line_header, &n.x, &n.y, &n.z);
             assert(matches == 4);
             normals.push_back(n);
-        } else if(strcmp(line_header, "usemtl") == 0) {
-            char name[256];
-            matches = sscanf(line, "%s %s\n", line_header, name);
+        }
+    }
+    original_data = file_data;
+
+    //
+    // Load mesh data
+    //
+    std::vector<Triangle>* mesh_triangles = &all_triangles[0] - 1;
+    MeshData* current_mesh = (scene->meshes - 1) + orig_num_meshes;
+    ModelData* current_model = (scene->models - 1) + orig_num_models;
+
+    const char* prev_line = NULL;
+    while(1) {
+        if(file_data == NULL)
+            break;
+        char line[1024] = {0};
+        const char* this_line = file_data;
+        file_data = (char*)get_line_from_buffer(line, sizeof(line), file_data);
+        const char* next_line = file_data;
+
+        char line_header[16] = {0};
+        sscanf(line, "%s", line_header);
+
+        if(strcmp(line_header, "usemtl") == 0) {
+            ++mesh_triangles;
+            ++current_mesh;
+            ++current_model;
+            // Get material name
+            char material_name[256];
+            matches = sscanf(line, "%s %s\n", line_header, material_name);
             assert(matches == 2);
-            all_indices.push_back(std::vector<int3>());
-            mesh_material_pairs.push_back(name);
-            current_indices++;
-        } else if(strcmp(line_header, "mtllib") == 0 ) {
-            char mtl_filename[256];
-            matches = sscanf(line, "%s %s\n", line_header, mtl_filename);
-            assert(matches == 2);
-            //_load_mtl_file(graphics, mtl_filename, all_materials);
+            // Check to see if this is named (a 'g' on the next or prev line)
+            if(prev_line && prev_line[0] == 'g') {
+                matches = sscanf(prev_line, "%s %s\n", line_header, current_mesh->name);
+                assert(matches == 2);
+            } else if(next_line && next_line[0] == 'g') {
+                matches = sscanf(next_line, "%s %s\n", line_header, current_mesh->name);
+                assert(matches == 2);
+            } else {
+                std::ostringstream s;
+                s << "mesh";
+                s << current_mesh - scene->meshes;
+                strncpy(current_mesh->name, s.str().c_str(), sizeof(current_mesh->name));
+            }
+            strncpy(current_model->mesh_name, current_mesh->name, sizeof(current_mesh->name));
+            strncpy(current_model->material_name, material_name, sizeof(material_name));
+
         } else if(strcmp(line_header, "f") == 0) {
             int3 triangle[4];
             if(textured) {
@@ -235,20 +437,27 @@ static void _load_obj(const char* filename)
                     matches = 13;
                 }
             }
-            std::vector<int3>& indices = all_indices[(size_t)current_indices];
-
-            indices.push_back(triangle[0]);
-            indices.push_back(triangle[1]);
-            indices.push_back(triangle[2]);
+            Triangle tri = {
+                triangle[0],
+                triangle[1],
+                triangle[2],
+            };
+            mesh_triangles->push_back(tri);
             if(matches == 13) {
-                indices.push_back(triangle[0]);
-                indices.push_back(triangle[2]);
-                indices.push_back(triangle[3]);
+                Triangle tri2 = {
+                    triangle[0],
+                    triangle[2],
+                    triangle[3],
+                };
+                mesh_triangles->push_back(tri2);
             }
         }
+        prev_line = this_line;
     }
-    free_file_data(original_data);
 
+    //
+    // Create meshes
+    //
     for(int jj=0; jj<(int)all_indices.size();++jj) {
         std::vector<int3>& indices = all_indices[(uint32_t)jj];
 
@@ -285,24 +494,16 @@ static void _load_obj(const char* filename)
 
         //Mesh* mesh = create_mesh(new_vertices, sizeof(Vertex)*vertex_count, &i[0], sizeof(uint32_t)*index_count, index_count);
 
-        FILE* mesh_file = fopen(mesh_names[(uint32_t)jj].c_str(), "wb");
-
-        // mtl_name_len
-        uint32_t material_name_len = (uint32_t)strlen(mesh_material_pairs[(uint32_t)jj].c_str());
-        fwrite(&material_name_len, 1, sizeof(material_name_len), mesh_file);
-        // mtl_name
-        fwrite(mesh_material_pairs[(uint32_t)jj].c_str(), 1, material_name_len, mesh_file);
-        // vertex count
-        fwrite(&vertex_count, 1, sizeof(vertex_count), mesh_file);
-        // index count
-        fwrite(&index_count, 1, sizeof(index_count), mesh_file);
-        // vertices
-        fwrite(new_vertices, sizeof(Vertex), vertex_count, mesh_file);
-        // indices
-        fwrite(&i[0], sizeof(uint32_t), index_count, mesh_file);
-
-        fclose(mesh_file);
-
+        std::string mesh_name;
+        if(jj >= (int)mesh_names.size()) {
+            static int mesh_number = 0;
+            std::ostringstream s;
+            mesh_name = "mesh";
+            s << mesh_number++;
+            mesh_name += s.str();
+        } else {
+            mesh_name = mesh_names[(uint32_t)jj];
+        }
 
         delete [] new_vertices;
 
@@ -321,14 +522,16 @@ static void _load_obj(const char* filename)
 //        }
 //        *num_materials = (int)all_materials.size();
 //    }
+    free_file_data(original_data);
 }
 
 /* External functions
  */
 int main(int argc, const char *argv[])
 {
+    SceneData scene = {0};
     for(int ii=1; ii<argc;++ii) {
-        _load_obj(argv[ii]);
+        _load_obj(argv[ii], &scene);
     }
     return 0;
 }
