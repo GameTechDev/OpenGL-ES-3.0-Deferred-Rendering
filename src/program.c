bc36b16c6d72d9b96e29c207cdd6f615a614ebc1
Kyle Weicht <kyle@awesomekyle.com>
2013-10-05 17:57:28 -0700

    Removing an assert

--- src/program.c
+++ src/program.c
@@ -93,7 +93,7 @@ Program create_program(const char* vertex_shader_filename,
         char message[1024];
         ASSERT_GL(glGetProgramInfoLog(program, sizeof(message), 0, message));
         system_log("Creating program: %s--%s failed: %s\n", vertex_shader_filename, fragment_shader_filename, message);
-        assert(link_status != GL_FALSE);
+        //assert(link_status != GL_FALSE);
         ASSERT_GL(glDetachShader(program, fragment_shader));
         ASSERT_GL(glDetachShader(program, vertex_shader));
         ASSERT_GL(glDeleteShader(fragment_shader));
